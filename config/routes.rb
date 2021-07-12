e579c4b9a7b1b7c10eac8d0e803e89b5ec0c5f46
Yuzuki-tech <employment1201@gmail.com>
2021-07-13 08:43:09 +0900

    Add a Home Page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home_pages/home'
+  get 'home_pages/tag'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'application#hello'
